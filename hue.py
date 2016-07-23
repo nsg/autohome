5a3d3ed9bccdf0f82d5d988482d40127db7b9d72
Stefan Berggren <nsg@nsg.cc>
2016-07-23 20:50:11 +0200

    Added locks and remove dead code.

--- hue.py
+++ hue.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 
+# Copyright (c) 2016 Stefan Berggren <nsg@nsg.cc>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from phue import Bridge
 import logging, datetime, time, os
 
@@ -13,17 +33,11 @@ class House:
     def lights(self):
         return self.hue.lights
 
-    def work_morning(self):
-        lamps = ['Bedroom', 'Hall spot 1']
-        self.hue.set_light(lamps, {
-            'transitiontime' : 6000,
-            'on' : True,
-            'bri' : 254}
-        )
+    def is_locked(self, lamp):
+        return os.path.isfile("/var/lib/hue/locks/{}".format(lamp.light_id))
 
-    def good_night(self):
-        for l in self.hue.lights:
-            l.on = False
+    def lock_file(self, lamp):
+        open("/var/lib/hue/locks/{}".format(lamp.light_id), 'a').close()
 
     def time_based_white(self, light):
         if not self.hue.get_light(light, 'on'): return
@@ -45,13 +59,10 @@ house = House()
 while True:
     s = time.strftime("%H:%M:%S")
 
-    # Morning light
-    if s == "07:55:30":
-        house.work_morning()
-
     # Every minute
     if datetime.datetime.now().second == 0:
         for l in house.lights():
-            house.time_based_white(l.name)
+            if not house.is_locked(l):
+                house.time_based_white(l.name)
 
     time.sleep(1)
